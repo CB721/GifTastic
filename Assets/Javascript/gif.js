266bf0addfedc44a98094fd3081c9500871e6134
CB721 <clintbrodar@gmail.com>
2019-05-03 08:42:06 -0700

    Pseudo code

---
+++ Assets/Javascript/gif.js
@@ -0,0 +1,15 @@
+//array of options - "topics"
+//display options as buttons
+//Giphy api link
+//on click event
+//call AJAX
+//get AJAX response
+//limit AJAX responses to 10
+//each on click should add an additional 10 gifs, not overwrite (optional)
+//gifs should start as non-animated
+//create var to push to gifView div
+//display rating of each gif
+//when user click gif, it plays
+//when the click gif again, it pauses
+//when user clicks the add button
+    //add to topics array
