e9e550d8b0ae3845b5a3747704c6f3461caeca52
CB721 <clintbrodar@gmail.com>
2019-05-03 20:29:59 -0700

    User input creates button

--- Assets/Javascript/gif.js
+++ Assets/Javascript/gif.js
@@ -1,42 +1,68 @@
 //initial topics and where user input will go
-let initialTopics = ["will arnett", "george clooney", "michael keaton", "adam west", "christian bale"];
+let initialTopics = ["will arnett", "george clooney", "michael keaton", "christian bale"];
 
 //create batman addition
 const batman = " batman";
 
-//array of options with "batman" added
+//array of topics with "batman" added
 var topics = [];
 
 //create variable to place combined strings in
 var combineStrings = " ";
 
-// add "batman" to the end of each string and push to topics array
-for (var i = 0; i < initialTopics.length; i++) {
-    //add "batman" to the end of each string in initialTopics array
-    combineStrings = initialTopics[i].concat(batman);
-    //add new string to "topics array"
-    topics.push(combineStrings);
-};
+//function to add to array
+function userAdditions() {
+    //prevent duplicate topics
+    topics = [];
+    // add "batman" to the end of each string and push to topics array
+    for (var i = 0; i < initialTopics.length; i++) {
+        //add "batman" to the end of each string in initialTopics array
+        combineStrings = initialTopics[i].concat(batman);
+        //add new string to "topics array"
+        topics.push(combineStrings);
+    };
+}
+userAdditions();
+
+//function to display buttons
+function buttonSetUp() {
+    //prevent duplicate buttons
+    $("#topicButton").empty();
+    //display topics as buttons
+    for (var j = 0; j < topics.length; j++) {
+        //generate button for each item in array
+        var a = $("<button>");
+        //add a class
+        a.addClass("topic");
+        //add a data attribute with the value of the topic
+        a.attr("data-person", topics[j]);
+        //add text to button with value of the topic
+        a.text(topics[j]);
+        //add button to HTML
+        $("#topicButton").append(a);
+    }
+}
+//call buttonSetUp function
+buttonSetUp();
 
 //add user input to initial topics array
-
-//display topics as buttons
-for (var j = 0; j < topics.length; j++) {
-    //generate button for each item in array
-    var a = $("<button>");
-    //add a class
-    a.addClass("topic");
-    //add a data attribute with the value of the topic
-    a.attr("data-person", topics[j]);
-    //add text to button with value of the topic
-    a.text(topics[j]);
-    //add button to HTML
-    $("#topicButton").append(a);
-    // $("#topicButton").append('<button>' + topics[j] + '</button>').val(topics[j]);
-}
+$("#add-gif").on("click", function (event) {
+    //prevent form from submitting itself
+    event.preventDefault();
+    //grab text from input box
+    var userInput = $("#gif-input").val().trim();
+    //add value from textbox to topics array
+    initialTopics.push(userInput);
+    //reinitialize userAdditions function
+    userAdditions();
+    //reinitialize buttonSetUp function
+    buttonSetUp();
+    console.log(topics);
+});
 
 //on click event
 $("button").on("click", function () {
+    console.log(this);
     //create variable based on attribute of which button was pressed
     var person = $(this).attr("data-person");
     //create url with name of the person
@@ -59,29 +85,29 @@ $("button").on("click", function () {
             // Looping over every result item
             for (var q = 0; q < results.length; q++) {
 
-                    // Push to html
-                    var gifDiv = $("#gifView");
+                // Push to html
+                var gifDiv = $("#gifView");
 
-                    // Storing the result item's rating
-                    var rating = results[q].rating;
+                // Storing the result item's rating
+                var rating = results[q].rating;
 
-                    // Creating a paragraph tag with the result item's rating
-                    var p = $("<p>").text("Rating: " + rating);
+                // Creating a paragraph tag with the result item's rating
+                var p = $("<p>").text("Rating: " + rating);
 
-                    // Creating an image tag
-                    var personImage = $("<img>");
+                // Creating an image tag
+                var personImage = $("<img>");
 
-                    // Giving the image tag an src attribute of a proprty pulled off the
-                    // result item
-                    personImage.attr("src", results[q].images.fixed_height.url);
+                // Giving the image tag an src attribute of a proprty pulled off the
+                // result item
+                personImage.attr("src", results[q].images.fixed_height.url);
 
-                    // Appending the paragraph and personImage we created to the "gifDiv" div we created
-                    gifDiv.append(p);
-                    gifDiv.append(personImage);
+                // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                gifDiv.append(p);
+                gifDiv.append(personImage);
 
-                    // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-                    $("#gifs-appear-here").prepend(gifDiv);
-                
+                // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                $("#gifs-appear-here").prepend(gifDiv);
+
             }
         });
 
@@ -90,9 +116,5 @@ $("button").on("click", function () {
 });
 
 //gifs should start as non-animated
-//create var to push to gifView div
-//display rating of each gif
 //when user click gif, it plays
 //when the click gif again, it pauses
-//when user clicks the add button
-    //add to topics array
