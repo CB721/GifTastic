b12df351b162730d2661dcb40ec6789c6a57c36b
CB721 <clintbrodar@gmail.com>
2019-05-03 19:28:53 -0700

    Create on click event for buttons

--- Assets/Javascript/gif.js
+++ Assets/Javascript/gif.js
@@ -1,15 +1,10 @@
 //initial topics and where user input will go
 let initialTopics = ["will arnett", "george clooney", "michael keaton", "adam west", "christian bale"];
 
-//display topics as buttons
-for (var j = 0; j < initialTopics.length; j++) {
-    $("#topicButton").append('<button>' + initialTopics[j] + '</button>');
-}
-
 //create batman addition
 const batman = " batman";
 
-//array of options - "topics"
+//array of options with "batman" added
 var topics = [];
 
 //create variable to place combined strings in
@@ -22,13 +17,39 @@ for (var i = 0; i < initialTopics.length; i++) {
     //add new string to "topics array"
     topics.push(combineStrings);
 };
-console.log(topics);
+//display topics as buttons
+for (var j = 0; j < topics.length; j++) {
+    //generate button for each item in array
+    var a = $("<button>");
+    //add a class
+    a.addClass("topic");
+    //add a data attribute with the value of the topic
+    a.attr("data-person", topics[j]);
+    //add text to button with value of the topic
+    a.text(topics[j]);
+    //add button to HTML
+    $("#topicButton").append(a);
+    // $("#topicButton").append('<button>' + topics[j] + '</button>').val(topics[j]);
+}
 
-//Giphy api link
 //on click event
-//call AJAX
-//get AJAX response
-//limit AJAX responses to 10
+$("button").on("click", function () {
+    //create variable based on attribute of which button was pressed
+    var person = $(this).attr("data-person");
+    //create url with name of the person
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        //attach api key and the person variable
+        //limit AJAX responses to 10
+        person + "&api_key=oZLjfolRARt1FqSzbHu9yHOboMpJ0CT7&limit=10";
+
+    // cal AJAX method and send URL to giphy
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+});
+
+
 //each on click should add an additional 10 gifs, not overwrite (optional)
 //gifs should start as non-animated
 //create var to push to gifView div
